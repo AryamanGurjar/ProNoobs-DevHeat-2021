7b2795f869f36d54f3506bf9e1afb6c3b98c6852
yash06112002 <ui20cs31@iiitsurat.ac.in>
2021-11-14 20:56:05 +0530

    Result added

--- script.js
+++ script.js
@@ -1,65 +1,78 @@
-let ans = 0, homeAmount = 0, educationAmount = 0, healthAmount = 0, travelAmount = 0, personalAmount = 0;
-function homeExpense() {
+let ans = 0, earning = 0, homeAmount = 0, educationAmount = 0, healthAmount = 0, travelAmount = 0, personalAmount = 0;
+
+function calculate() {
+    var x = (document.getElementById("earning").value);
+    earning += parseInt(x);
+
     var x = document.getElementsByClassName("housebill");
     var i;
     for (i = 0; i < x.length; i++) {
         homeAmount += parseInt(x[i].value);
-        // ans += parseInt(x[i].value);
+        ans += parseInt(x[i].value);
     }
-    // console.log(res);
-    // console.log(ans);
-    document.getElementById("para").innerHTML = ` amount is ${ans}`;
 
-}
-function educationExpense() {
     var x = document.getElementsByClassName("edubill");
     var i;
     for (i = 0; i < x.length; i++) {
         educationAmount += parseInt(x[i].value);
-        // ans += parseInt(x[i].value);
+        ans += parseInt(x[i].value);
     }
-    // console.log(res);
-    // console.log(ans);
-    document.getElementById("para").innerHTML = ` amount is ${ans}`;
 
-}
-function healthExpense() {
+
     var x = document.getElementsByClassName("healthbill");
     var i;
     for (i = 0; i < x.length; i++) {
         healthAmount += parseInt(x[i].value);
-        // ans += parseInt(x[i].value);
+        ans += parseInt(x[i].value);
     }
-    // console.log(res);
-    // console.log(ans);
-    document.getElementById("para").innerHTML = ` amount is ${ans}`;
 
-}
-function travellingExpense() {
+
     var x = document.getElementsByClassName("travelbill");
     var i;
     for (i = 0; i < x.length; i++) {
         travelAmount += parseInt(x[i].value);
-        // ans += parseInt(x[i].value);
+        ans += parseInt(x[i].value);
     }
-    // console.log(res);
-    // console.log(ans);
-    document.getElementById("para").innerHTML = ` amount is ${ans}`;
 
-}
-function personalExpense() {
+
     var x = document.getElementsByClassName("personalbill");
     var i;
     for (i = 0; i < x.length; i++) {
         personalAmount += parseInt(x[i].value);
-        // ans += parseInt(x[i].value);
+        ans += parseInt(x[i].value);
     }
-    // console.log(res);
-    // console.log(ans);
-    document.getElementById("para").innerHTML = ` amount is ${ans}`;
+    document.getElementById("anspara").innerHTML = ` Household Expenses are ${homeAmount}<br>
+     Education Expenses are ${educationAmount}<br>
+     Health Expenses are ${healthAmount}<br>
+     Travel Expenses are ${travelAmount}<br>
+     Personal Expenses are ${personalAmount}<br>
+     Earnings are ${earning} <br>
+    Savings are ${earning - ans} <br> `;
+
 }
-function result() {
-    ans = homeAmount + healthAmount + travelAmount + personalAmount + educationAmount;
-    document.getElementById("para").innerHTML = `Total amount is ${ans}`;
+
+
+window.onclick = function () {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title: {
+            text: "Your Expenses"
+        },
+        data: [{
+            type: "pie",
+            startAngle: 0,
+            yValueFormatString: "##0.00\"%\"",
+            indexLabel: "{label} {y}",
+            dataPoints: [
+                { y: homeAmount, label: "homeAmount" },
+                { y: educationAmount, label: "educationAmount" },
+                { y: healthAmount, label: "healthAmount" },
+                { y: travelAmount, label: "travelAmount" },
+                { y: personalAmount, label: "personalAmount" },
+            ]
+        }]
+    });
+    chart.render();
 
 }
